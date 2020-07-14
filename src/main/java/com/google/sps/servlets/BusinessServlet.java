74ee47412a1834736d0d199e029e36335c4cbbc3
Sara <kolbs141@gmail.com>
2020-07-14 14:22:08 -0700

    ran lint

--- src/main/java/com/google/sps/servlets/BusinessServlet.java
+++ src/main/java/com/google/sps/servlets/BusinessServlet.java
@@ -1,9 +1,10 @@
 package com.google.sps.servlets;
 
 import com.google.gson.Gson;
-import java.io.IOException;
+// import com.opencsv.*;
+import com.opencsv.CSVReader;
 import java.io.FileReader;
-import com.opencsv.*;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
@@ -15,44 +16,43 @@ import javax.servlet.http.HttpServletResponse;
 public class BusinessServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      String bus= csvToJson("/home/skolb/software-product-sprint/team13/src/main/webapp/businesses.csv");
-      response.setContentType("application/json;");
-      response.getWriter().println(bus);
+    String bus =
+        csvToJson("/home/skolb/software-product-sprint/team13/src/main/webapp/businesses.csv");
+    response.setContentType("application/json;");
+    response.getWriter().println(bus);
   }
 
-private String csvToJson(String file) 
-{ //return json of all rows
-    List<String[]> json= new ArrayList<String[]>();
-	try { 
+  // from https://www.geeksforgeeks.org/reading-csv-file-java-using-opencsv/
+  //changes csv to json for all rows
+  private String csvToJson(String file) { // return json of all rows
+    List<String[]> json = new ArrayList<String[]>();
+    try {
+      // Create an object of filereader
+      // class with CSV file as a parameter.
+      FileReader filereader = new FileReader(file);
+      // create csvReader object passing
+      // file reader as a parameter
+      CSVReader csvReader = new CSVReader(filereader);
+      String[] nextRecord;
 
-		// Create an object of filereader 
-		// class with CSV file as a parameter. 
-		FileReader filereader = new FileReader(file); 
-		// create csvReader object passing 
-		// file reader as a parameter 
-		CSVReader csvReader = new CSVReader(filereader); 
-		String[] nextRecord; 
-
-		// we are going to read data line by line 
-		while ((nextRecord = csvReader.readNext()) != null) { 
-			for (String cell : nextRecord) { 
-				System.out.print(cell + "\t");                  
-			} 
-            json.add(nextRecord);
-			System.out.println(); 
-		} 
-	} 
-	catch (Exception e) { 
-		e.printStackTrace(); 
-	} 
-
+      // we are going to read data line by line
+      while ((nextRecord = csvReader.readNext()) != null) {
+        for (String cell : nextRecord) {
+          System.out.print(cell + "\t");
+        }
+        json.add(nextRecord);
+        System.out.println();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
     return convertToJson(json);
-} 
+  }
 
-private String convertToJson(List<String[]> toBeConverted) {
+  private String convertToJson(List<String[]> toBeConverted) {
     Gson gson = new Gson();
     String json = gson.toJson(toBeConverted);
-    System.out.println("convertToJson(): json= "+json);
+    System.out.println("convertToJson(): json= " + json);
     return json;
   }
 }
