a6e8a018aad34fb8937d6e517407bd4342db8f90
Pranjali Sharma <ps3072@columbia.edu>
2020-07-14 17:04:04 +0530

    add cards to donations page

--- src/main/webapp/donations.js
+++ src/main/webapp/donations.js
@@ -1,14 +1,27 @@
 function getOrgs() {
-  console.log('inside getOrgs()');
   fetch('/donations').then(response => response.json()).then((json) => {
-    console.log(json);
     const charities = json.charities;
-    console.log(charities);
-    const cardsEl = document.getElementById('orgs row');
     for (let i = 1; i < charities.length; i++) {
-      console.log('json element name ' + i + ' : ' + charities[i].name);
-      console.log('json element blurb ' + i + ' : ' + charities[i].blurb);
-      console.log('json element link ' + i + ' : ' + charities[i].link);
+        var id= charities[i].id;
+        var name= charities[i].name;
+        var blurb_txt= charities[i].blurb;
+        var link= charities[i].link;
+        var new_card=$("<div class='card' id="+id+">")  
+        var card_body=$("<div class='card-body row'>")  
+        var title=$("<h5 class='card-title'>")   
+        $(title).text(name);     
+        var blurb=$("<p class='card-text'>")
+        $(blurb).text(blurb_txt);
+        var button=$("<a href="+link+" class='btn btn-warning'>")
+        $(button.text("Donate here!"));
+
+    $("#add-deck").append(new_card)
+    $(new_card).append(card_body)    
+    $(card_body).append(title)
+    $(card_body).append(blurb)  
+    $(card_body).append(button)  
+
     }
   });
 }
+
